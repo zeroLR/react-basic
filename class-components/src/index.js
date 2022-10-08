1a0fcee2803297292bec9c38eb5e1431efcdbde7
ceyang <ceyanglab@gmail.com>
2022-10-09 01:12:54 +0800

    class-components: turn into class component

--- class-components/src/index.js
+++ class-components/src/index.js
@@ -5,21 +5,26 @@ function formatDate(date) {
   return date.toLocaleDateString();
 }
 
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img
-          className="Avatar"
-          src={props.author.avatarUrl}
-          alt={props.author.name}
-        />
-        <div className="UserInfo-name">{props.author.name}</div>
+class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="Comment">
+        <div className="UserInfo">
+          <img
+            className="Avatar"
+            src={this.props.author.avatarUrl}
+            alt={this.props.author.name}
+          />
+          <div className="UserInfo-name">{this.props.author.name}</div>
+        </div>
+        <div className="Comment-text">{this.props.text}</div>
+        <div className="Comment-date">{formatDate(this.props.date)}</div>
       </div>
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">{formatDate(props.date)}</div>
-    </div>
-  );
+    );
+  }
 }
 
 const comment = {
