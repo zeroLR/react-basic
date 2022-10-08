57ace84aac1b8cec4c68664edec6cec5647184b4
ceyang <ceyanglab@gmail.com>
2022-10-09 01:36:45 +0800

    state-lifecycle: add local state to class

--- state-lifecycle/src/index.js
+++ state-lifecycle/src/index.js
@@ -5,20 +5,17 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 class Clock extends React.Component {
   constructor(props) {
     super(props);
+    this.state = { date: new Date() };
   }
 
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     );
   }
 }
 
-function tick() {
-  root.render(<Clock date={new Date()} />);
-}
-
-setInterval(tick, 1000);
+root.render(<Clock />);
