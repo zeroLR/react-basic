70ab3d106146234f64a28a2236496eec96139356
ceyang <ceyanglab@gmail.com>
2022-10-08 23:32:05 +0800

    function-components: separate out UserInfo component

--- function-components/src/index.js
+++ function-components/src/index.js
@@ -12,13 +12,19 @@ function Avatar(props) {
   );
 }
 
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">{props.user.name}</div>
+    </div>
+  );
+}
+
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <Avatar user={props.author} />
-        <div className="UserInfo-name">{props.author.name}</div>
-      </div>
+      <UserInfo user={props.author} />
       <div className="Comment-text">{props.text}</div>
       <div className="Comment-date">{formatDate(props.date)}</div>
     </div>
