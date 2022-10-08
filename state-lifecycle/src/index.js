f160a590639331d409a945567338c58027d09f38
ceyang <ceyanglab@gmail.com>
2022-10-09 01:33:24 +0800

    state-lifecycle: turn function to class component

--- state-lifecycle/src/index.js
+++ state-lifecycle/src/index.js
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
-function Clock(props) {
-  return (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {props.date.toLocaleTimeString()}.</h2>
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
 }
 
 function tick() {
